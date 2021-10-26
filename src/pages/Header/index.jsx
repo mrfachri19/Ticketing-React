32d2557e7b87ce99c3f3080017b08b0545e3af54
fachri <fachryfachry1997@gmail.com>
2021-10-26 12:01:57 +0700

    update login

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -1,66 +1,62 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, NavDropdown, Button, Container} from "react-bootstrap";
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import "./index.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, NavDropdown, Button, Container } from "react-bootstrap";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./index.css";
 import tiket from "../../assets/image/Vector.png";
-
-
-
 
 // import { Link, withRouter } from "react-router-dom";
 
 class Header extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       newName: props.name
-//     };
-//   }
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       newName: props.name
+  //     };
+  //   }
 
   handleHome = () => {
     console.log("home");
-    this.props.history.push("/basic-login");
+    this.props.history.push("/login");
   };
 
   render() {
     // console.log(this.props);
     // console.log(this.state);
     return (
-      <>
-      <Navbar bg="white" expand="lg">
-      <Container className="container">
-        <Navbar.Brand ><img src={tiket} alt="ticketz 1" /></Navbar.Brand>
-        <Nav className="navbar__brand">
-        <Nav.Link onClick={this.handleHome}>Home</Nav.Link>
-        <Nav.Link href="#link">Profile</Nav.Link>
-        <Nav.Link href="#link">Payment</Nav.Link>
-        </Nav>
-        
-        <Navbar.Collapse className="nav justify-content-end">
-          <Nav className="justify-content-end ">
-            <NavDropdown title="Location" className="justify-content-end">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="" ><FontAwesomeIcon icon={faSearch} /></Nav.Link>
-            <Button variant="primary" className="navbar__button">Sign In</Button>{' '}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </>
-        
-          
-          
-           
-        
-     
-      
+      <div className="container">
+        <Navbar bg="white" expand="lg">
+          <Container className="container">
+            <Navbar.Brand>
+              <img src={tiket} alt="ticketz 1" />
+            </Navbar.Brand>
+            <Nav className="navbar__brand">
+              <Nav.Link onClick={this.handleHome}>Home</Nav.Link>
+              <Nav.Link href="#link">Profile</Nav.Link>
+              <Nav.Link href="#link">Payment</Nav.Link>
+            </Nav>
+
+            <Navbar.Collapse className="nav justify-content-end">
+              <Nav className="justify-content-end ">
+                <NavDropdown title="Location" className="justify-content-end">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="">
+                  <FontAwesomeIcon icon={faSearch} />
+                </Nav.Link>
+                <Button variant="primary" className="navbar__button">
+                  Sign In
+                </Button>{" "}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     );
   }
 }
--- src/pages/auth/Login/index.jsx
+++ src/pages/auth/Login/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, Jumbotron } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form, Jumbotron } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import axios from "../../../utils/axios";
 import tiket from "../../../assets/image/tickitz 1.png";
 
@@ -59,67 +59,76 @@ class Login extends Component {
     // console.log("Reset Form");
   };
 
-
-
-
   render() {
     return (
-    
-       <div className="container">
-        
-        <div className="row">
-          <div className="col-sm-8">
+      <div className="row">
+        <div className="col-sm-8">
           <Jumbotron>
-            <div className="jumbotron text-center tiket"> 
+            <div className="jumbotron text-center tiket">
               <img src={tiket} alt="tickitz 1" />
               <h1 className="display-4">Wait, Watch, Wow !</h1>
-            </div> 
+            </div>
           </Jumbotron>
-              <div className="jumbotron__img">
-              <img src={tiket} alt="tickitz 1" />
-              </div>
+          <div className="jumbotron__img">
+            <img src={tiket} alt="tickitz 1" />
           </div>
-        
-          <div className="col-sm-4 form__login">
+        </div>
 
-            <div className="row ">
-              <h1>Sign In</h1>
-              <p>Sign in with your data that you entered during <br /> your registration</p>
-            </div>
-            <div className="row login">
-              <Form onSubmit={this.handleSubmit} onReset={this.handleReset}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email </Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" name="email" onChange={this.handleChangeForm} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" name="password" onChange={this.handleChangeForm} />
-                </Form.Group>
-                <div className="d-grid gap-2 form__button">
-                  <Button variant="primary" type="submit" size="lg" >
-                    Sign In
-                  </Button>
-                </div>
-              </Form>
-            </div>
-            <div className="row text-center form__login-reset">
-              <p>Forgot Your Password? <Link to ='www.google.com'>Reset Now</Link> </p>
-            </div>
-            <div className="row button__login">
-              <div className="col-sm-6 form__login-btnlink text-center">
-              <Button variant="light"> <p>Google</p> </Button> 
-              </div>
-              <div className="col-sm-6 form__login-btnlink text-center">
-              <Button variant="light"> <p>Facebook</p> </Button>
-              </div>
-            </div>
-          
+        <div className="col-sm-4 form__login">
+          <div className="row ">
+            <h1>Sign In</h1>
+            <p>
+              Sign in with your data that you entered during <br /> your registration
+            </p>
           </div>
-
-        </div>  
+          <div className="row login">
+            <Form onSubmit={this.handleSubmit} onReset={this.handleReset}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  onChange={this.handleChangeForm}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={this.handleChangeForm}
+                />
+              </Form.Group>
+              <div className="d-grid gap-2 form__button">
+                <Button variant="primary" type="submit" size="lg">
+                  Sign In
+                </Button>
+              </div>
+            </Form>
+          </div>
+          <div className="row text-center form__login-reset">
+            <p>
+              Forgot Your Password? <Link to="www.google.com">Reset Now</Link>{" "}
+            </p>
+          </div>
+          <div className="row button__login">
+            <div className="col-sm-6 form__login-btnlink text-center">
+              <Button variant="light">
+                {" "}
+                <p>Google</p>{" "}
+              </Button>
+            </div>
+            <div className="col-sm-6 form__login-btnlink text-center">
+              <Button variant="light">
+                {" "}
+                <p>Facebook</p>{" "}
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-      
     );
   }
 }
